16500f007b0c7d9923c41772129560d27c61697e
Daryan Ibe <daryan@t-ibe.de>
2023-12-15 17:16:31 +0100

    Initial commit

---
+++ src/main/java/net/dtp/endmod/EndModDTPClient.java
@@ -0,0 +1,10 @@
+package net.dtp.endmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EndModDTPClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
